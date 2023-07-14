8ca19dc50fa31499778035d05d7aa0b464e28f89
Gurleen <gurleen2@myseneca.ca>
2023-07-14 04:53:11 -0400

    initial Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const resultHandler = () => {
+    //..
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <h2>Display</h2>
+      <table>
+
+        <tr>
+          <td><button value='7' >7</button></td>
+          <td><button value='8' >8</button></td>
+          <td><button value='9' >9</button></td>
+          <td><button value='divide'>/</button></td>
+        </tr>
+        <tr>
+          <td><button value='4' >4</button></td>
+          <td><button value='5' >5</button></td>
+          <td><button value='6'>6</button></td>
+          <td><button value='multiply' >x</button></td>
+        </tr>
+        <tr>
+          <td><button value='1' >1</button></td>
+          <td><button value='2' >2</button></td>
+          <td><button value='3' >3</button></td>
+          <td><button value='subtract'>-</button></td>
+        </tr>
+        <tr>
+          <td><button>+</button></td>
+          <td><button value='0'>0</button></td>
+          <td><button>C</button></td>
+          <td><button onClick={resultHandler}>=</button></td>
+        </tr>
+      </table>
     </div>
   );
 }
